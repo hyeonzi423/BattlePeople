96a2d696e2f1c85e38af2316122fe220c05138b0
Yangseyeon <seyeon0207@gmail.com>
2024-07-15 10:59:01 +0900

    init project

---
+++ battle-people-server/src/test/java/com/woowahanrabbits/battle_people/BattlePeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowahanrabbits.battle_people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlePeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
