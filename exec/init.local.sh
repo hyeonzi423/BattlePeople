4297b7ff8b14b90f64bf01a9ec25360fd12388d0
윤성하 <dbstjdgk1541@gmail.com>
2024-08-16 12:23:38 +0900

    Update init.local.sh

--- exec/init.local.sh
+++ exec/init.local.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 cd $SCRIPT_DIR
-sudo docker compose -f docker-compose.yml -f docker-compose.local.override.yml up -d
+sudo docker compose -f docker-compose.local.override.yml up -d
